46f3896702bf817b6aede2608430918607d0787b
Philip J Freeman <elektron@halo.nu>
2025-06-20 14:09:27 -0700

    Use boto3 for uploads...

--- ServerBackupS3/defaults.py
+++ ServerBackupS3/defaults.py
@@ -17,42 +17,6 @@
 
 """
 
-# S3 MULTIPART Settings
-####################
-#
-# Limitations:
-#  * Multipart uploads can have at most 10000 parts.
-#  * Objects can be up to 5TB in total size.
-#
-# so, MAX_S3_OBJECT_SIZE / MAX_S3_OBJECT_SIZE must be less than 10000.
-#
-# (https://aws.amazon.com/blogs/aws/amazon-s3-multipart-upload/)
-#
-
-# MAX_S3_OBJECT_SIZE
-#   defines the maximum object size we upload to s3
-
-MAX_S3_OBJECT_SIZE = 1024*1024*1024*1024 # 1TB
-
-# CHUNK_SIZE
-#   defines the size of multi-part "chunks" we upload to s3
-
-CHUNK_SIZE = 1024*1024*128 # 128MiB
-
-# DEFAULT_S3_RETRY_TIMEOUT
-#   time to wait, in seconds, before re attempting an S3 connection in failure
-#   state.
-#
-
-DEFAULT_S3_RETRY_TIMEOUT = 60
-
-# DEFAULT_S3_FAILURE_TIMEOUT
-#   after timeout, in seconds, give up and let backup fail
-#
-
-DEFAULT_S3_FAILURE_TIMEOUT = 7200
-
-
 # Path Defaults
 ###############
 #
